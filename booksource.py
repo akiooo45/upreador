88b7c2e8a653888a5a180ac217f4bf1970939835
akiooo45 <huoji302562020@gmail.com>
2024-03-25 16:54:02 +0800

    add delete local booksource

--- booksource.py
+++ booksource.py
@@ -1,34 +1,46 @@
 import requests
 import json
+import urllib.parse
 
 
-username = "admin"  # 账号
-password = "admin"  # 密码
-
-session = requests.Session()
+username = "admin"
+password = "admin"
+remote_booksource = "https://jihulab.com/aoaostar/legado/-/raw/release/cache/1b8256c78b385543b5e8aa6a0d7693c76f8e60d4.json"
+reader_addr = "http://127.0.0.1:4396"
+session = requests.session()
 
 
 def login():
-    url = "http://127.0.0.1:4396/reader3/login"  # reader地址
+    login_url = "/reader3/login"
+    url = urllib.parse.urljoin(reader_addr, login_url)
+    # print(url)
     data = {"username": username, "password": password, "isLogin": True}
     headers = {
         "Content-Type": "application/json",
     }
-    re = session.post(url, data=json.dumps(data), headers=headers)
-    print(json.dumps(re.json(), sort_keys=True, indent=4))
+    session.post(url, data=json.dumps(data), headers=headers).json()
 
 
-def get_bs():
-    bs_url = "https://jihulab.com/aoaostar/legado/-/raw/release/cache/71e56d4f1d8f1bff61fdd3582ef7513600a9e108.json"  # 书源地址
-    return requests.get(bs_url).json()
+def deleteBookSource():
+    deleteBookSource_url = "/reader3/deleteAllBookSources"
+    url = urllib.parse.urljoin(reader_addr, deleteBookSource_url)
+    print(session.post(url).json())
+
+
+def get_remotebs():
+    return requests.get(remote_booksource).json()
 
 
 def send_bs():
-    reader_addr = "http://127.0.0.1:4396/reader3/saveBookSources/"  # reader地址
-    data = get_bs()
-    headers = {"Content-Type": "application/json"}
-    print(session.post(url=reader_addr, headers=headers, json=data).json())
+    remote_booksource = "/reader3/saveBookSources"
+    url = urllib.parse.urljoin(reader_addr, remote_booksource)
+    headers = {
+        "Content-Type": "application/json",
+    }
+    data = get_remotebs()
+    print(session.post(url, json=data, headers=headers).json())
 
 
 login()
+deleteBookSource()
 send_bs()
